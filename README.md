3ea826a5391db3a9ff54cc100373ad13c77b8a4f
Okke Harsta <oharsta@zilverline.com>
2019-06-05 08:22:54 +0200

    Generate Tink KeySet

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # OpenConext-crypto
+
+Generate TINK cryptographic key material.  
+
+First fetch the crypto jar with all dependencies packaged inside the jar.
+```
+wget https://build.openconext.org/repository/public/releases/org/openconext/crypto/1.0.0/crypto-1.0.0-shaded.jar
+```
+Then generate the key material and output to the console:
+```
+java -jar crypto-1.0.0-shaded.jar
+```
+Or output to a file:
+```
+java -jar crypto-1.0.0-shaded.jar secret-keyset.json
+```
---
+++ src/main/java/crypto/TinkWrapper.java
@@ -0,0 +1,24 @@
+package crypto;
+
+import com.google.crypto.tink.CleartextKeysetHandle;
+import com.google.crypto.tink.JsonKeysetWriter;
+import com.google.crypto.tink.KeysetHandle;
+import com.google.crypto.tink.aead.AeadConfig;
+import com.google.crypto.tink.aead.AeadKeyTemplates;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
+
+public class TinkWrapper {
+
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+        OutputStream outputStream = args != null && args.length == 1 ? new FileOutputStream(args[0]) : System.out;
+        AeadConfig.register();
+        KeysetHandle keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES256_CTR_HMAC_SHA256);
+        CleartextKeysetHandle.write(keysetHandle, JsonKeysetWriter.withOutputStream(outputStream));
+    }
+
+}
